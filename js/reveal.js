c1d91ced5c9d072c4acd70aac0e998468ffd772e
dominopizzaaaa <domksj23@gmail.com>
2026-02-25 21:14:17 +0800

    Add modules

--- js/reveal.js
+++ js/reveal.js
@@ -10,10 +10,10 @@
       // Stagger siblings within same parent
       const siblings = [...e.target.parentElement.querySelectorAll('.reveal-up, .reveal-left')];
       const idx = siblings.indexOf(e.target);
-      setTimeout(() => e.target.classList.add('visible'), idx * 80);
+      setTimeout(() => e.target.classList.add('visible'), idx * 40);
       obs.unobserve(e.target);
     });
-  }, { threshold: 0.10, rootMargin: '0px 0px -40px 0px' });
+  }, { threshold: 0.05, rootMargin: '0px 0px -40px 0px' });
 
   ELS.forEach(el => obs.observe(el));
 })();
